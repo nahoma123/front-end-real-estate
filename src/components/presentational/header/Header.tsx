118ede63f58b75e208c86ba6fc9a67667ce98f2b
Nahom Asnake <nahomasnake12@gmail.com>
2023-10-10 21:12:08 +0300

    header added

--- src/components/presentational/header/Header.tsx
+++ src/components/presentational/header/Header.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import { Link,BrowserRouter } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Grid,
+  styled,
+} from "@mui/material";
+import NotificationsIcon from "@mui/material/Icon";
 
-function Header() {
+const StyledAppBar = styled(AppBar)({
+  backgroundColor: "#033E6B",
+  color: "#fff",
+  boxShadow: "none",
+});
+
+const StyledLogo = styled(Typography)`
+  && {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #fff;
+    text-decoration: none;
+  }
+`;
+
+const StyledSellButton = styled(Button)`
+  && {
+    background-color: #ff5722;
+    color: #fff;
+    &:hover {
+      background-color: #e64a19;
+    }
+  }
+`;
+
+const Header: React.FC = () => {
+
   return (
-    <AppBar position="static">
+    <StyledAppBar position="static">
       <Toolbar>
-        <BrowserRouter>
-        <Button component={Link} to="/" color="inherit">
-          Home
-        </Button>
-        <Button component={Link} to="/about" color="inherit">
-          About
-        </Button>
-        </BrowserRouter>
-        {/* Add other navigation links */}
+        <Grid container  component="div">
+          <Grid md={6} sm={6}>
+            {/* docs */}
+            <StyledLogo variant="h6">Strike</StyledLogo>
+          </Grid>
+          <Grid md={6} sm={6}>
+            {/* docs */}
+            <StyledSellButton>Sell Your House</StyledSellButton>
+            <IconButton color="inherit">
+              <NotificationsIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
       </Toolbar>
-    </AppBar>
+    </StyledAppBar>
   );
-}
+};
 
 export { Header };
